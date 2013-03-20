017bbc170d6c8b41c4bf42e6f137f817cf5395a8
Björn Dahlgren <bjodah@gmail.com>
2013-03-20 19:39:25 +0100

    Working installation via setup.py

--- cinterpol/tests/test_core.py
+++ cinterpol/tests/test_core.py
@@ -1,31 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+# for use with e.g. py.test
 import numpy as np
 import matplotlib.pyplot as plt
 
-from c_interpol import PieceWisePolyInterpol
+from cinterpol import PieceWisePolyInterpol
 
 
-def test_Traj():
-    c = np.array([[1, 3, 2], [6.0, 0, 0]]) # y=1+3x+2*x**2
-    t = np.array([0.0, 1.0])
-    tj = Traj(t, c)
-    print tj([0.0, 0.5, 1.0])
-
-def test_Traj_mk_from_data():
+def test_PieceWisePolyInterpol_mk_from_data():
     t = np.array([0.0, 1.0])
     y = np.array([[0.0, 0.0, 0.0],
                   [1.0, 3.0, 6.0]]) # y=x**3
-    t = Traj.mk_from_data(t, y)
-    print t.get_t()
-    print t.get_c()
-    plot_t = np.linspace(0, 1.0, 50)
-    plot_y = t(plot_t)
-    plt.plot(plot_t, plot_y, label = 'Interpol')
-    plt.plot(plot_t, plot_t ** 3, label = 'Analytic', ls='--')
-    plt.legend()
-    plt.show()
-
-if __name__ == '__main__':
-    test_Traj()
-    test_Traj_mk_from_data()
+    t = PieceWisePolyInterpol.mk_from_data(t, y)
+    assert np.allclose(t.get_t(), t)
+    assert np.allclose(t.get_c(), np.array(
+        [[0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+         [1.0, 3.0, 6.0, 0.0, 0.0, 0.0]]))
+    t_series = np.linspace(0, 1.0, 50)
+    interpol_y = t(t_series)
+    exact_y = t_series ** 3
+    assert np.allclose(exact_y, interpol_y)
 
