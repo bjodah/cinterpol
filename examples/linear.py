1cc783e819005b9cd35dfd527682c9827ab03bb5
Björn Dahlgren <bjodah@gmail.com>
2013-03-27 14:37:04 +0100

    everything working

--- examples/linear.py
+++ examples/linear.py
@@ -2,5 +2,6 @@ import numpy as np
 import cinterpol
 
 def test_PieceWisePolyInterpol_call():
-    pwpi=cinterpol.PieceWisePolyInterpol.mk_from_array(np.array([0.0,1.0]),np.array([[0.0],[1.0]]))
+    pwpi=cinterpol.PieceWisePolyInterpol.mk_from_array(np.array([0.0,1.0,2.0]),np.array([[0.0],[1.0],[2.0]]))
     assert pwpi(0.5) == 0.5
+    assert pwpi(1.5) == 1.5
--- examples/sin.py
+++ examples/sin.py
@@ -5,9 +5,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import cPickle as pickle
+
 from cinterpol import PieceWisePolyInterpol
 
-def main(start=0, stop=20, Ncoarse=20, Nfine=1000):
+
+def main(start=0, stop=20, Ncoarse=21, Nfine=1000):
     """
     """
     x = np.linspace(start, stop, Ncoarse)
@@ -15,15 +18,21 @@ def main(start=0, stop=20, Ncoarse=20, Nfine=1000):
     y1 =  np.cos(x)
     y2 = -np.sin(x)
     all_y = np.ascontiguousarray(np.vstack((y0, y1, y2)).transpose())
-    print all_y.shape
-    pw = PieceWisePolyInterpol(x, all_y)
+    pw = PieceWisePolyInterpol.mk_from_array(x, all_y)
 
+    # Demo serialization
+    pickle.dump(pw, open('pw.pkl', 'wb'))#, protocol = 2)
+    del pw
+    pw = pickle.load(open('pw.pkl', 'rb'))
     xfine = np.linspace(start, stop, Nfine)
     ipy = pw(xfine)
-
+    plt.subplot(2, 1, 1)
     plt.plot(x, y0, '*', label = 'Data')
     plt.plot(xfine, ipy, label='Interpolated')
     plt.plot(xfine, np.sin(xfine), label ='Analytic')
+    plt.legend()
+    plt.subplot(2, 1, 2)
+    plt.plot(xfine, np.sin(xfine) - ipy, label='Error')
     plt.legend()
     plt.show()
 
