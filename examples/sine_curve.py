Compiles but numerically incorrect

examples/sine_curve.py
@@ -30,7 +30,7 @@ def get_cos_pp(start=0, stop=pi7, Ncoarse=21):
     return PiecewisePolynomial(x, all_y)
 
 
-def plot(pp, t0=None, tend=None, Nfine=4000, analytic_cb=np.sin,
+def plot(pp, deriv=0, t0=None, tend=None, Nfine=4000, analytic_cb=np.sin,
          plot_rows=2, plot_cols=1, plot_idx=1, show=True, title=None):
     """
     """
@@ -40,12 +40,12 @@ def plot(pp, t0=None, tend=None, Nfine=4000, analytic_cb=np.sin,
     xfine = np.linspace(t0, tend, Nfine)
     plt.subplot(plot_rows, plot_cols, plot_idx)
     plt.plot(pp.t, pp.c[:,0], '*', label = 'Function evaluated points')
-    plt.plot(xfine, pp(xfine), label='Interpolated')
+    plt.plot(xfine, pp(xfine, deriv=deriv), label='Interpolated')
     plt.plot(xfine, analytic_cb(xfine), label ='Analytic')
     plt.legend()
     if title: plt.title(title)
     plt.subplot(plot_rows, plot_cols, plot_idx+1)
-    plt.plot(xfine, analytic_cb(xfine) - pp(xfine),
+    plt.plot(xfine, analytic_cb(xfine) - pp(xfine, deriv=deriv),
              label='Absolute error')
     plt.legend()
     if show: plt.show()
@@ -61,16 +61,13 @@ if __name__ == '__main__':
     pp = get_sin_pp()
     plot(pp, plot_rows=4, plot_cols=2, show=False, title='sin(x) : analytic: sin(x)')
 
-    Dpp = pp.derivative()
-    plot(Dpp, analytic_cb=np.cos, plot_rows=4, plot_cols=2,
+    plot(pp, deriv=1, analytic_cb=np.cos, plot_rows=4, plot_cols=2,
          plot_idx=3, show=False, title='D_x sin(x) : analytic: cos(x)')
 
-    DDpp = pp.derivative(2)
-    plot(DDpp, analytic_cb=minus_sin, plot_rows=4, plot_cols=2,
+    plot(pp, deriv=2, analytic_cb=minus_sin, plot_rows=4, plot_cols=2,
          plot_idx=5, show=False, title='D_x^2 sin(x) : analytic: -sin(x)')
 
-    DDDpp = pp.derivative(3)
-    plot(DDDpp, analytic_cb=minus_cos, plot_rows=4, plot_cols=2,
+    plot(pp, deriv=3, analytic_cb=minus_cos, plot_rows=4, plot_cols=2,
          plot_idx=7, show=False, title='D_x^3 sin(x) : analytic: -cos(x)')
 
     plt.show()